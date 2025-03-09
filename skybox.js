40a95895adfc145d5266e803b183bc4fc3bc3059
System141 <thesystem141@gmail.com>
2025-03-10 01:11:59 +0300

    Rename Skybox to SkySphere and implement sky sphere creation with gradient shader

--- skybox.js
+++ skybox.js
@@ -1,19 +1,42 @@
-import * as THREE from '/node_modules/three/build/three.module.js';
-import SkyboxGenerator from './skyboxGenerator.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
 
-export default class Skybox {
+export default class SkySphere {
     constructor(scene) {
         this.scene = scene;
-        this.skyboxGenerator = new SkyboxGenerator();
-        this.createSkybox();
+        this.createSkySphere();
     }
 
-    createSkybox() {
-        const textures = this.skyboxGenerator.generateSkybox();
-        const materialArray = textures.map(texture => new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide }));
-
-        const skyboxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
-        const skybox = new THREE.Mesh(skyboxGeometry, materialArray);
-        this.scene.add(skybox);
+    createSkySphere() {
+        const geometry = new THREE.SphereGeometry(500, 60, 40);
+        const material = new THREE.ShaderMaterial({
+            uniforms: {
+                topColor: { value: new THREE.Color(0x87CEEB) },
+                bottomColor: { value: new THREE.Color(0xFFFFFF) },
+                offset: { value: 33 },
+                exponent: { value: 0.6 }
+            },
+            vertexShader: `
+                varying vec3 vWorldPosition;
+                void main() {
+                    vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+                    vWorldPosition = worldPosition.xyz;
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+                }
+            `,
+            fragmentShader: `
+                uniform vec3 topColor;
+                uniform vec3 bottomColor;
+                uniform float offset;
+                uniform float exponent;
+                varying vec3 vWorldPosition;
+                void main() {
+                    float h = normalize(vWorldPosition + offset).y;
+                    gl_FragColor = vec4(mix(bottomColor, topColor, max(pow(max(h, 0.0), exponent), 0.0)), 1.0);
+                }
+            `,
+            side: THREE.BackSide
+        });
+        const skySphere = new THREE.Mesh(geometry, material);
+        this.scene.add(skySphere);
     }
 }
